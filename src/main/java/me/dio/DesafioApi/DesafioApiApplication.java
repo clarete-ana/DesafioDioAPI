4d3479dae7d2d914b33e9f790a4490dc6c7ecbd0
Clarete-ana <anaclaraclarete2@gmail.com>
2025-05-12 16:37:03 -0300

    First commit

---
+++ src/main/java/me/dio/DesafioApi/DesafioApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.DesafioApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/DesafioApi/DesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.DesafioApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
